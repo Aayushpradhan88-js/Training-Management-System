ae69f39b6bbb07cef00a1d5e9a6cef8b5dcdbbbb
Aayush <AAYUSHPRADHAN996@outlook.com>
2025-12-02 11:11:22 +0545

    database connection and config file writing

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -7,7 +7,12 @@ config();
 function fileConfig(){
     return {
         PORT: process.env.PORT || 3000,
-    }
-}
+        dbName:process.env.DB_NAME,
+        dbUsername:process.env.DB_USERNAME,
+        dbPassword:process.env.DB_PASSWORD,
+        dbHost:process.env.DB_HOST,
+        dbPort:process.env.DB_PORT
+    };
+};
 
 export {fileConfig};
--- server/src/database/connnection.ts
+++ server/src/database/connnection.ts
@@ -1,2 +1,11 @@
-import {Sequelize} from 'sequelize'
+import { Sequelize } from 'sequelize'
+import { fileConfig } from '../config/config.ts'
 
+const schema = new Sequelize({
+    database: fileConfig().dbName,
+    username: fileConfig().dbUsername,
+    password: fileConfig().dbPassword,
+    host: fileConfig().dbPort,
+    dialect: "mysql",
+    port: Number(fileConfig().dbPort),
+});
