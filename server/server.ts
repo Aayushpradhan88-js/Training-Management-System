3f635760249df084c7be00c5ad3db644dfdec661
Aayush <AAYUSHPRADHAN996@outlook.com>
2025-12-02 11:17:55 +0545

    importing to the main file

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,9 @@
 
 import app from "./src/app.ts"
 import fileConfig from "./src/config/config.ts";
+import sequalizeDB from "./src/database/connnection.ts";
+
+sequalizeDB
 
 function startServer() {
     const port = fileConfig().PORT;
