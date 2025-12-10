18842bb6937634f817f9f1fecf7578b9ac01efc8
Aayush <AAYUSHPRADHAN996@outlook.com>
2025-12-11 04:43:55 +0545

    adding try catch block to resolve error

--- backend/src/modules/features/institute/instituteController.ts
+++ backend/src/modules/features/institute/instituteController.ts
@@ -7,7 +7,7 @@ const createInstitute = async (req: Request, res: Response) => {
     const { instituteName, instituteEmail, institutePhoneNumber, instituteAddress } = req.body;
     const { instituteVatNumber } = req.body || null;
     const { institutePanNumber } = req.body || null;
-    console.log("details",instituteName, instituteEmail, institutePhoneNumber, instituteAddress )
+    console.log("details", instituteName, instituteEmail, institutePhoneNumber, instituteAddress)
 
     if (!instituteName || !instituteEmail || !institutePhoneNumber || !instituteAddress) {
         return res.status(400).json({
@@ -15,24 +15,28 @@ const createInstitute = async (req: Request, res: Response) => {
         })
     };
 
-    await sequelize.query(`CREATE TABLE IF NOT EXIST(
-        id VARCHAR(36) PRIMARY KEY DEFAULT (UUID()),
-        instituteName VARCHAR(225) NOT NULL, 
-        instituteEmail VARCHAR(225) NOT NULL, 
-        institutePhoneNumber VARCHAR(225) NOT NULL, 
-        instituteAddress VARCHAR(225) NOT NULL,
-        instituteVatNumber VARCHAR(225) , 
-        institutePanNumber VARCHAR(225) ,
-        createdAt TIMESTAMP DEFAULT CURRENT_TIME ,
-        updatedAt TIMESTAMP DEFAULT CURRENT_TIME ON UPDATE CURRENT_TIMESTAMP
-        )`);
+    try {
+        await sequelize.query(`CREATE TABLE IF NOT EXIST(
+         id VARCHAR(36) PRIMARY KEY DEFAULT (UUID()),
+         instituteName VARCHAR(225) NOT NULL, 
+         instituteEmail VARCHAR(225) NOT NULL, 
+         institutePhoneNumber VARCHAR(225) NOT NULL, 
+         instituteAddress VARCHAR(225) NOT NULL,
+         instituteVatNumber VARCHAR(225) , 
+         institutePanNumber VARCHAR(225) ,
+         createdAt TIMESTAMP DEFAULT CURRENT_TIME ,
+         updatedAt TIMESTAMP DEFAULT CURRENT_TIME ON UPDATE CURRENT_TIMESTAMP
+         )`);
 
-    // make a database for institute and teacher
+        // make a database for institute and teacher
 
 
-    res.status(200).json({
-        message: "Institute created"
-    })
+        res.status(200).json({
+            message: "Institute created"
+        });
+    } catch (error) {
+        console.error("failed to create institute server error", (error as Error).stack);
+    };
 };
 
-export {createInstitute};
+export { createInstitute };
