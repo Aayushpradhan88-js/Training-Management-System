632fe866125d3354ba4860785e9fa5bca8f08284
Aayush <AAYUSHPRADHAN996@outlook.com>
2025-12-11 04:27:09 +0545

    injecting institute route file to the main file

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import authRouter from './modules/global/auth/authRouter';
+import instituteRouter from './modules/features/institute/instituteRoutes';
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
+app.use("/api/institute", instituteRouter);
 
 export default app;
