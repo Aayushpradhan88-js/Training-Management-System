0cce34db7ea131483a59e299d8826f1c1df5f07e
Aayush <AAYUSHPRADHAN996@outlook.com>
2025-12-12 17:32:51 +0545

    fix: JWT module issue

--- backend/src/modules/features/institute/instituteRoutes.ts
+++ backend/src/modules/features/institute/instituteRoutes.ts
@@ -1,9 +1,10 @@
 import express from 'express'
-import {createInstitute} from './instituteController'
+import { createInstitute } from './instituteController'
+import userVerification from '../../global/middleware/authMiddleware';
 
 const instituteRouter = express.Router();
 
 console.log("✅ step 3: CREATED INSTITUTE ROUTER STARTED")
-instituteRouter.route("/").post(createInstitute);
+instituteRouter.route("/").post(userVerification.userAuthorizationAccessVerification, createInstitute);
 
 export default instituteRouter;
--- backend/src/modules/global/middleware/authMiddleware.ts
+++ backend/src/modules/global/middleware/authMiddleware.ts
@@ -1,17 +1,27 @@
-import { Request, Response } from "express";
-import {verify} from "jsonwebtoken";
+import { NextFunction, Request, Response } from "express";
+import * as jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../../../config/env";
 
 class userVerification{
-    static userAuthorizationAccessVerification(req:Request, res:Response) {
+    static userAuthorizationAccessVerification(req:Request, res:Response, next:NextFunction) {
         console.log("✅ step 14: Token triggered");
         const token= req.headers.authorization;
-        const isValidToken = verify(( token as string), JWT_SECRET, (success, error) => {
+        console.log("✅ step 18: Token validation completed", token);
+        const isValidToken = jwt.verify(( token as string), JWT_SECRET, (success, error) => {
             if(success) {
                 console.log("✅ step 15: Token validation completed");
             } else {
                 console.error("✅ step 16: Invalid token", (error as string));
             }
         });
+
+        console.log("✅ step 17: Getting decoded token", isValidToken);
+
+        // const user = isValidToken.user;
+        // req.user = user;
+
+        next()
     }
-}
+};
+
+export default userVerification;
