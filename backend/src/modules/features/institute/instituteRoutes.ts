d38b5f5c0e4f8476d2d4312060969772819db982
Aayush <AAYUSHPRADHAN996@outlook.com>
2025-12-14 18:41:40 +0545

    code push

--- backend/src/modules/features/institute/instituteRoutes.ts
+++ backend/src/modules/features/institute/instituteRoutes.ts
@@ -4,9 +4,8 @@ import instituteController from './instituteController';
 
 const instituteRouter = express.Router();
 
-// console.log("✅ step 3: CREATED INSTITUTE ROUTER STARTED")
 instituteRouter.route("/").post(
-    userVerification.userAuthorizationAccessVerification,
+    userVerification.userAuthorizationAccessVerification,//middleware implemented before institute creation
     instituteController.createInstitute
 );
 
