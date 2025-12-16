61674b4b621eb7f15384a23c36a1f6ff165a6078
Aayush <AAYUSHPRADHAN996@outlook.com>
2025-12-16 14:42:55 +0545

    comment out

--- backend/src/modules/global/services/asyncErrorHandler.ts
+++ backend/src/modules/global/services/asyncErrorHandler.ts
@@ -2,13 +2,13 @@
 
 import { NextFunction, Request, Response } from "express"
 
-console.log("✅error handler code triggered")
+// console.log("✅error handler code triggered")
 class GlobalErrorHandler {
     static asyncErrorHandler<T = void>(fn: (req: Request, res: Response, next: NextFunction) => Promise<T>) {
         return (req: Request, res: Response, next: NextFunction) => {
-            console.log("✅success message triggered")
+            // console.log("✅success message triggered")
             fn(req, res, next).catch((error: Error) => {
-                console.error("✅ERROR code launched", error);
+                console.error("ERROR", error);
                 return res.status(500).json({
                     message: error.message,
                     fullError: error
