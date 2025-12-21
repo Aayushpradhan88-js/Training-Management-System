3b5cc042e586d96f037c9b0301ce9379dc3efe4d
Aayush <AAYUSHPRADHAN996@outlook.com>
2025-12-21 15:55:59 +0545

    push

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -8,12 +8,18 @@ const DB_NAME = process.env.DB_NAME as string
 const DB_USERNAME = process.env.DB_USERNAME as string
 const DB_PASSWORD = process.env.DB_PASSWORD as string
 const DB_HOST = process.env.DB_HOST as string
-const DB_PORT = Number(process.env.DB_PORT)|| 3306
-const JWT_SECRET=process.env.JWT_SECRET! as string
-const JWT_EXPIRY=process.env.JWT_EXPIRY || '5d'
+const DB_PORT = Number(process.env.DB_PORT) || 3306
+const JWT_SECRET = process.env.JWT_SECRET! as string
+const JWT_EXPIRY = process.env.JWT_EXPIRY || '5d'
+const CLOUDINARY_NAME = process.env.CLOUDINARY_NAME as string
+const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY as string
+const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET as string
+
+
 
 export {
     SERVER_PORT,
     DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT,
-    JWT_SECRET,JWT_EXPIRY
+    JWT_SECRET, JWT_EXPIRY,
+    CLOUDINARY_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET
 }
