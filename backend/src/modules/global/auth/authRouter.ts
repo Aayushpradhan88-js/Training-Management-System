3ae2e7d0ebbd3ec5d469dccca236c564ddc24ab7
Aayush <AAYUSHPRADHAN996@outlook.com>
2025-12-22 15:28:06 +0545

    comment out code

--- backend/src/modules/global/auth/authRouter.ts
+++ backend/src/modules/global/auth/authRouter.ts
@@ -5,7 +5,7 @@ const authRouter = Router();
 
 authRouter.route("/register").post(GlobalErrorHandler.asyncErrorHandler(AuthController.registerUser));
 authRouter.route("/login").post(GlobalErrorHandler.asyncErrorHandler(AuthController.loginUser));
-authRouter.route("/logout").post();
-authRouter.route("/forgetpassword").post();
+// authRouter.route("/logout").post();
+// authRouter.route("/forgetpassword").post();
 
 export default authRouter;
