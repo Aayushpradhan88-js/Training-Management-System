06196013f50464b26a0874a1f5a6c4560d4053c6
Aayush <AAYUSHPRADHAN996@outlook.com>
2025-12-22 16:07:21 +0545

    add seed data for categories

---
+++ backend/src/modules/seed.ts
@@ -0,0 +1,20 @@
+const categories = [
+    {
+        categoryName: "web_development",
+        categoryDescription: "AI powered web development"
+    },
+    {
+        categoryName: "backend_development",
+        categoryDescription: "Backend development involves server-side logic, APIs, and technologies such as Node.js, Express, Python, Django, or Java Spring for handling data and application functionality."
+    },
+    {
+        categoryName: "database_management",
+        categoryDescription: "Database management includes SQL (MySQL, PostgreSQL) and NoSQL (MongoDB) databases, querying, modeling, and optimization for efficient data storage and retrieval."
+    },
+    {
+        categoryName: "devops_and_deployment",
+        categoryDescription: "DevOps and deployment covers tools like Git, Docker, Kubernetes, CI/CD pipelines, and cloud platforms (AWS, Azure) for version control, automation, and scalable application hosting."
+    },
+];
+
+export default categories;
