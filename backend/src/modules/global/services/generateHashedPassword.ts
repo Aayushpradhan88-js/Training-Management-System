4d347b012dbaf7a57de8a77461ae6c48d83fbbed
Aayush <AAYUSHPRADHAN996@outlook.com>
2025-12-24 16:38:59 +0545

    push

--- backend/src/modules/global/services/generateHashedPassword.ts
+++ backend/src/modules/global/services/generateHashedPassword.ts
@@ -5,6 +5,8 @@ import * as bcrypt from 'bcrypt'
 class generateHashedPasswordService{
     static async genereateHashPassword(teacherName:string){
         const randomPassword = Math.floor(10000 + Math.random() * 30000)
-        bcrypt.hash(`${randomPassword} ${teacherName}, 10`);
+        await bcrypt.hash(`${randomPassword} ${teacherName}`, 12);
+
+        
     }
 }
