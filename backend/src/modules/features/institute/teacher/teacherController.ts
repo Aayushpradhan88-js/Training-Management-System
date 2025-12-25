6ba9e0a83bc53b8d14c99066e081c24a68003d45
Aayush <AAYUSHPRADHAN996@outlook.com>
2025-12-25 14:09:18 +0545

    teacher controller code completed

--- backend/src/modules/features/institute/teacher/teacherController.ts
+++ backend/src/modules/features/institute/teacher/teacherController.ts
@@ -3,22 +3,28 @@ import IExtendedRequest from "../../../global/types/types";
 import sequelize from "../../../../database/connection";
 import { Query } from "mysql2/typings/mysql/lib/protocol/sequences/Query";
 import { QueryTypes } from "sequelize";
+import generateRandomPasswordService from "../../../global/services/generateRandomPassword";
 
 class TeacherController {
     static async createTeacher(req: IExtendedRequest, res: Response) {
         const currentInstituteNumber = req.user?.currentInstituteNumber;
-        if (!currentInstituteNumber || currentInstituteNumber.trim().length === 0) {
+        if (!currentInstituteNumber || currentInstituteNumber?.trim().length === 0) {
             return res.status(400).json({ errorMessage: "Invalid institute number" });
         };
 
-        const { teacherName, teacherEmail, teacherPassword, teacherPhoneNumber, teacherExperience, joinedDate, salary, teacherPhoto, courseId } = req.body;
-        if (!teacherName || !teacherEmail || !teacherPassword || !teacherPhoneNumber || !teacherExperience || !joinedDate || !salary || !teacherPhoto || !courseId) {
+        const { teacherName, teacherEmail, teacherPassword, teacherPhoneNumber, teacherExperience, joinedDate, salary, courseId } = req.body;
+        if (!teacherName || !teacherEmail || !teacherPassword || !teacherPhoneNumber || !teacherExperience || !joinedDate || !salary || !courseId) {
             return res.status(400).json({ errorMessage: "fill all the fields" });
-        }
+        };
 
-        
+        const passwordData = generateRandomPasswordService.genereateHashPassword(teacherName);
+        if(!passwordData){
+            return res.status(500).json({ errorMessage: "password hashing failed" });
+        };
 
-        await sequelize.query(`
+        const teacherPhoto: string = req.file? req.file.path : "https://img.freepik.com/premium-vector/silver-membership-icon-default-avatar-profile-icon-membership-icon-social-media-user-image-vector-illustration_561158-4195.jpg?semt=ais_hybrid&w=740&q=80"
+
+        const data = await sequelize.query(`
                 INSERT TO  teacher_${currentInstituteNumber}(
                     teacherName,
                     teacherEmail,
@@ -27,15 +33,19 @@ class TeacherController {
                     teacherExperience,
                     joinedDate,
                     salary,
-                    teacherPhoto,
                     courseId
                 ) VALUES(?,?,?,?,?,?,?,?,?)
             `, {
             type: QueryTypes.INSERT,
-            replacements: [teacherName, teacherEmail, teacherPassword, teacherPhoneNumber, teacherExperience, joinedDate, salary, teacherPhoto, courseId]
+            replacements: [teacherName, teacherEmail, (await passwordData).hashPassword, teacherPhoneNumber, teacherExperience, joinedDate, salary, teacherPhoto, courseId]
         });
 
 
+        return res.status(200).json({
+            datas: data,
+            success: true,
+            message:"teacher created successfully"
+        })
     };
 };
 
---
+++ backend/src/modules/global/services/generateRandomPassword.ts
@@ -0,0 +1,17 @@
+import { Response } from "express";
+import IExtendedRequest from "../types/types";
+import * as bcrypt from 'bcrypt'
+
+class generateRandomPasswordService{
+    static async genereateHashPassword(teacherName:string){
+        const randomPassword = Math.floor(10000 + Math.random() * 30000)
+        const passwordData = {
+            hashPassword: await bcrypt.hash(`${randomPassword} ${teacherName}`, 12), //stored in database
+            plainPassword: `${randomPassword} ${teacherName}` //gives to teacher plain password
+        };
+
+        return passwordData;
+    };
+};
+
+export default generateRandomPasswordService;
