47e9002320da463de819d4e2f18aa569624f3a2c
Aayush <AAYUSHPRADHAN996@outlook.com>
2025-12-25 14:13:30 +0545

    adding routes for testing feature

--- backend/src/modules/features/institute/teacher/teacherRoutes.ts
+++ backend/src/modules/features/institute/teacher/teacherRoutes.ts
@@ -1,1 +1,13 @@
 import express from 'express'
+import GlobalErrorHandler from '../../../global/services/asyncErrorHandler';
+import TeacherController from './teacherController';
+import UserVerification from '../../../global/middleware/authMiddleware';
+import upload from '../../../global/middleware/cloudinaryMiddleware';
+const teacherRouter = express.Router();
+
+teacherRouter.route("/create").post(
+    UserVerification.userAuthorizationAccessVerification,
+    upload.single('teacher photo'),
+    GlobalErrorHandler.asyncErrorHandler(TeacherController.createTeacher))
+
+export default teacherRouter;
